eb8241c44319bebf42f1830e4824aadb0e5f8f1a
ftrbnd <giosalas25@gmail.com>
2026-02-25 21:29:19 -0800

    Remove location details query

--- src/components/location-preview.tsx
+++ src/components/location-preview.tsx
@@ -1,9 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import {
-  singleLocationPhotoQueryOptions,
-  singleLocationQueryOptions,
-} from '@/services/tripadvisor/query-options'
+import { singleLocationPhotoQueryOptions } from '@/services/tripadvisor/query-options'
 import { Location } from '@/services/tripadvisor/schema'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { Bookmark, Eye } from 'lucide-react'
@@ -14,14 +11,13 @@ interface LocationPreviewProps {
 }
 
 export function LocationPreview({ city, location }: LocationPreviewProps) {
-  const { data: details } = useSuspenseQuery(
-    singleLocationQueryOptions(city, location.location_id),
-  )
+  // const { data: details } = useSuspenseQuery(
+  //   singleLocationQueryOptions(city, location.location_id),
+  // )
+
   const { data: photo } = useSuspenseQuery(
     singleLocationPhotoQueryOptions(city, location.location_id),
   )
-
-  if (!details) return null
 
   return (
     <Card className="relative mx-auto w-full max-w-sm pt-0 hover:cursor-pointer">
@@ -31,7 +27,7 @@ export function LocationPreview({ city, location }: LocationPreviewProps) {
         className="relative aspect-video w-full object-cover   dark:brightness-40 rounded-t-md"
       />
       <CardHeader>
-        <CardTitle>{details.name}</CardTitle>
+        <CardTitle>{location.name}</CardTitle>
       </CardHeader>
       <CardFooter className="flex-col gap-2">
         <Button className="w-full">
