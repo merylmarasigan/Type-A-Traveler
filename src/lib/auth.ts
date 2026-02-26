603fd40ab27df1e8362ea4bb87066b933dad618e
ftrbnd <giosalas25@gmail.com>
2026-02-25 19:44:42 -0800

    Add google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { betterAuth } from 'better-auth'
 import { username } from 'better-auth/plugins'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { serverEnv } from '@/config/env'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -18,4 +19,13 @@ export const auth = betterAuth({
     }),
     tanstackStartCookies(),
   ],
+  baseURL: serverEnv.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: serverEnv.GOOGLE_CLIENT_ID,
+      clientSecret: serverEnv.GOOGLE_CLIENT_SECRET,
+      accessType: 'offline',
+      prompt: 'select_account consent',
+    },
+  },
 })
