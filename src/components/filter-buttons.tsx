8f3d208def59d3b007b7c3b47e7df6b452a1aa4d
Jacob-Parcell <jakeparcell14@gmail.com>
2026-02-28 16:22:16 -0800

    Added Working Radio Buttons

--- src/components/filter-buttons.tsx
+++ src/components/filter-buttons.tsx
@@ -1,23 +1,39 @@
-import { useState } from 'react';
-
-import { Button } from '@/components/ui/button'
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldLabel,
+  FieldTitle,
+} from "@/components/ui/field"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 export default function FilterButtons() {
-  
-  const [testText, settestText] = useState("Pressed");
-
-  // function handleClick() {
-  //   settestText(testText => testText+", Pressed");
-  // };
-
-  function handleClick() {
-    settestText(prev => prev+", Pressed");
-  };
-
   return (
-    <div className="">
-      <Button onClick={handleClick}>Test</Button>
-      <p>{testText}</p>
-    </div>
+    <RadioGroup defaultValue="hotels" className="grid-flow-col">
+      <FieldLabel htmlFor="hotels">
+        <Field orientation="horizontal">
+          <FieldContent>
+            <FieldTitle>Hotels</FieldTitle>
+          </FieldContent>
+          <RadioGroupItem value="hotels" id="hotels" className="sr-only"/>
+        </Field>
+      </FieldLabel>
+      <FieldLabel htmlFor="attractions">
+        <Field orientation="horizontal">
+          <FieldContent>
+            <FieldTitle>Attractions</FieldTitle>
+          </FieldContent>
+          <RadioGroupItem value="attractions" id="attractions" className="sr-only"/>
+        </Field>
+      </FieldLabel>
+      <FieldLabel htmlFor="restaurants">
+        <Field orientation="horizontal">
+          <FieldContent>
+            <FieldTitle>Restaurants</FieldTitle>
+          </FieldContent>
+          <RadioGroupItem value="restaurants" id="restaurants" className="sr-only"/>
+        </Field>
+      </FieldLabel>
+    </RadioGroup>
   )
 }
