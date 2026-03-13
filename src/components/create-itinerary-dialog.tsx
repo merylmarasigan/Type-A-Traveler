43705175f65155eeb352d8919202f041fd6e133c
ftrbnd <giosalas25@gmail.com>
2026-03-12 17:00:29 -0700

    Add option to select an existing itinerary during creation

--- src/components/create-itinerary-dialog.tsx
+++ src/components/create-itinerary-dialog.tsx
@@ -9,15 +9,67 @@ import {
 import { Button } from '@/components/ui/button'
 import { useSavedActivities } from '@/hooks/use-saved-activities'
 import { DateRangePicker } from '@/components/date-range-picker'
+import { useItineraryFolders } from '@/hooks/use-itinerary-folders'
+import { authClient } from '@/lib/auth-client'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { useItineraryDays } from '@/hooks/use-itinerary-days'
+import { useRouter } from '@tanstack/react-router'
+import { addDays } from 'date-fns'
+import { useState } from 'react'
+import { DateRange } from 'react-day-picker'
+import { Spinner } from '@/components/ui/spinner'
+import { CalendarCheck2, ChevronDown } from 'lucide-react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible'
+import { ItineraryFolder } from '@/db/types'
 
 interface CreateItineraryDialogProps {
   city: string
 }
 
 export function CreateItineraryDialog({ city }: CreateItineraryDialogProps) {
+  const { data } = authClient.useSession()
   const { userActivitiesQuery } = useSavedActivities({ city })
+  const { foldersQuery } = useItineraryFolders(data?.user.id, 50)
+  const { createInitialDays, createIsPending } = useItineraryDays()
+  const router = useRouter()
+
+  const [dateRange, setDateRange] = useState<DateRange>({
+    from: new Date(),
+    to: addDays(new Date(), 14),
+  })
+  const [selectedItinerary, setSelectedItinerary] =
+    useState<ItineraryFolder | null>(null)
 
   const noSavedActivities = userActivitiesQuery.data.length === 0
+
+  const handleConfirmDates = async () => {
+    if (selectedItinerary) {
+      // TODO: implement adding to existing itinerary
+    } else {
+      const { itineraryFolder } = await createInitialDays(dateRange, city)
+
+      await router.navigate({
+        to: '/itineraries/$id',
+        params: { id: itineraryFolder.id },
+      })
+    }
+  }
+
+  const handleSelectItinerary = (id: string) => {
+    const folder = foldersQuery.data.find((folder) => folder.id === id) ?? null
+    setSelectedItinerary(folder)
+  }
 
   return (
     <Dialog>
@@ -31,7 +83,50 @@ export function CreateItineraryDialog({ city }: CreateItineraryDialogProps) {
             What days will you be traveling?
           </DialogDescription>
         </DialogHeader>
-        <DateRangePicker city={city} disabled={noSavedActivities} />
+
+        <div className="flex flex-col gap-2 px-4 pb-4">
+          <DateRangePicker
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+            disabled={noSavedActivities}
+          />
+
+          {foldersQuery.data.length > 0 && (
+            <Collapsible className="flex flex-col gap-2">
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" className="w-full">
+                  Add to an existing itinerary?
+                  <ChevronDown />
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <Select onValueChange={handleSelectItinerary}>
+                  <SelectTrigger className="w-full p-2">
+                    <SelectValue placeholder="Select an itinerary" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {foldersQuery.data.map((folder) => (
+                        <SelectItem key={folder.id} value={folder.id}>
+                          {folder.title}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </CollapsibleContent>
+            </Collapsible>
+          )}
+
+          <Button disabled={createIsPending} onClick={handleConfirmDates}>
+            {createIsPending ? <Spinner /> : <CalendarCheck2 />}
+            {createIsPending
+              ? 'Saving...'
+              : selectedItinerary
+                ? `Add to ${selectedItinerary.title}`
+                : 'Confirm'}
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   )
--- src/components/date-range-picker.tsx
+++ src/components/date-range-picker.tsx
@@ -1,40 +1,23 @@
-import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
-import { addDays, formatDate } from 'date-fns'
-import { CalendarCheck2 } from 'lucide-react'
-import { useState } from 'react'
+import { formatDate } from 'date-fns'
+import { Dispatch, SetStateAction } from 'react'
 import { type DateRange } from 'react-day-picker'
-import { useItineraryDays } from '@/hooks/use-itinerary-days'
-import { Spinner } from '@/components/ui/spinner'
-import { useRouter } from '@tanstack/react-router'
 import { Card, CardContent } from '@/components/ui/card'
 import { TypographyMuted, TypographySmall } from '@/components/ui/typography'
 
 interface DateRangePickerProps {
-  city: string
+  dateRange: DateRange
+  setDateRange: Dispatch<SetStateAction<DateRange>>
   disabled?: boolean
 }
 
-export function DateRangePicker({ city, disabled }: DateRangePickerProps) {
-  const [dateRange, setDateRange] = useState<DateRange>({
-    from: new Date(),
-    to: addDays(new Date(), 14),
-  })
-
-  const { createInitialDays, createIsPending } = useItineraryDays()
-  const router = useRouter()
-
-  const handleConfirmDates = async () => {
-    const { itineraryFolder } = await createInitialDays(dateRange, city)
-
-    await router.navigate({
-      to: '/itineraries/$id',
-      params: { id: itineraryFolder.id },
-    })
-  }
-
+export function DateRangePicker({
+  dateRange,
+  setDateRange,
+  disabled,
+}: DateRangePickerProps) {
   return (
-    <div className="flex flex-col gap-3 px-4 pb-4">
+    <>
       <Card>
         <CardContent>
           <Calendar
@@ -62,13 +45,6 @@ export function DateRangePicker({ city, disabled }: DateRangePickerProps) {
           )}
         </TypographyMuted>
       )}
-      <Button
-        disabled={disabled || createIsPending}
-        onClick={handleConfirmDates}
-      >
-        {createIsPending ? <Spinner /> : <CalendarCheck2 />}
-        {createIsPending ? 'Saving...' : 'Confirm'}
-      </Button>
-    </div>
+    </>
   )
 }
