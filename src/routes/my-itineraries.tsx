ef8e6851f3c3f97cee661d22e1e067bb47fc9578
Meryl Marasigan <meryl0729@gmail.com>
2026-03-25 21:00:28 -0700

    implemented itinerary filtering based on userId

--- src/routes/my-itineraries.tsx
+++ src/routes/my-itineraries.tsx
@@ -9,25 +9,29 @@ import {
 import { Button } from '@/components/ui/button'
 import { useItineraryFolders } from '@/hooks/use-itinerary-folders'
 import { MySqlInsertBase } from 'drizzle-orm/mysql-core'
+import { authClient } from '@/lib/auth-client'
 
 export const Route = createFileRoute('/my-itineraries')({
   component: myItinerariesPage,
 })
 
 function myItinerariesPage() {
-  const { foldersQuery } = useItineraryFolders()
-  const { data: folders } = foldersQuery
+    const{data} = authClient.useSession();
+  const { userFoldersQuery } = useItineraryFolders(data?.user?.id)
+  const { data: folders } = userFoldersQuery
+
+  console.log(data?.user?.id)
 
   return (
     <div>
       <h1 className="ml-5 mb-3 mt-5 text-7xl font-bold">My Itineraries</h1>
-      <h2 className="ml-5 mb-10">Where past, present, and future trip plans live!</h2>
+      <h2 className="ml-5 mb-10">Where your past, present, and future trip plans live!</h2>
       <div className="grid grid-cols-3 gap-5 m-5">
-        {folders.map((folder) => (
+        {folders
+        .map((folder) => (
           <Card key={folder.id}>
             <CardHeader>
               <CardTitle>{folder.title}</CardTitle>
-              <p className="text-muted-foreground text-sm">{folder.authorId}</p>
               <CardDescription>{folder.description}</CardDescription>
             </CardHeader>
             <CardFooter className="justify-end">
