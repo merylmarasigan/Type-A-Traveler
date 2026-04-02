c727261ebd273b65a63a74b9159d13062b1d4cac
ftrbnd <giosalas25@gmail.com>
2026-04-01 20:41:13 -0700

    Create folder-preview component and add link to folder page

---
+++ src/components/itineraries/itinerary-folder-preview.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from '@/components/ui/card'
+import { ItineraryFolder } from '@/db/types'
+import { useCityItineraries } from '@/hooks/use-city-itineraries'
+import { Link } from '@tanstack/react-router'
+
+interface ItineraryFolderPreviewProps {
+  folder: ItineraryFolder
+}
+
+export function ItineraryFolderPreview({
+  folder,
+}: ItineraryFolderPreviewProps) {
+  const { itinerariesQuery } = useCityItineraries(folder.id)
+
+  const cityCount = itinerariesQuery.data.length
+  const multipleCities = itinerariesQuery.data.length > 1
+  const firstCity = itinerariesQuery.data[0]
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{multipleCities ? folder.title : firstCity.title}</CardTitle>
+        <CardDescription>{folder.description}</CardDescription>
+      </CardHeader>
+      {multipleCities && <CardContent>{cityCount} cities</CardContent>}
+      <CardFooter className="justify-end">
+        <Button
+          asChild
+          size="sm"
+          className="bg-blue-500 hover:bg-blue-600 text-white"
+        >
+          <Link to="/itineraries/$id" params={{ id: folder.id }}>
+            View
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
