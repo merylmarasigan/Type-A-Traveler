9a8f5c681732dd12dd2c69b1050ab60975475f3d
Giovanni Salas <giosalas25@gmail.com>
2026-04-16 16:04:17 -0700

    Fix image src path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,21 +9,24 @@ import {
 } from '@/components/ui/navigation-menu'
 import { AuthHeader } from '@/components/auth/header-user'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { Button } from '@/components/ui/button'
 
 export function Header() {
   return (
     <header className="p-4 w-full">
       <div className="flex justify-between items-center w-full">
-        <Link to="/">
-          <Image
-            src={`type_a_traveler_logo.png`}
-            layout="constrained"
-            width={192}
-            height={48}
-            alt="Type A Traveler"
-            className="dark:invert hover:invert-90 dark:hover:invert-90"
-          />
-        </Link>
+        <Button asChild variant="ghost">
+          <Link to="/">
+            <Image
+              src="/type_a_traveler_logo.png"
+              layout="constrained"
+              width={192}
+              height={48}
+              alt="Type A Traveler"
+              className="dark:invert hover:invert-30 dark:hover:invert-90"
+            />
+          </Link>
+        </Button>
         <div className="flex items-center gap-4">
           <NavigationMenu>
             <NavigationMenuList>
